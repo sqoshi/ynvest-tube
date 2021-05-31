49da84bf63ad986c4f74711c7609f8028afc2ec5
wolfthewizard <matis306@gmail.com>
2021-05-31 13:10:43 +0200

    tied bidding to ui

--- ynvest_tube/app/src/main/java/com/app/ynvest_tube/activities/AuctionActivity.kt
+++ ynvest_tube/app/src/main/java/com/app/ynvest_tube/activities/AuctionActivity.kt
@@ -2,6 +2,8 @@ package com.app.ynvest_tube.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.app.ynvest_tube.R
@@ -13,46 +15,71 @@ import com.app.ynvest_tube.repository.Repository
 class AuctionActivity : AppCompatActivity() {
 
     private val repository = Repository()
+    private var auctionId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auction)
 
-        val auctionId = intent.getIntExtra(Auction::id.name, 0)
+        auctionId = intent.getIntExtra(Auction::id.name, 0)
 
-        if(auctionId == 0)
+        if (auctionId == 0)
             finish()
 
         repository.getActionDetails(::auctionDetailsObtained, ::requestFailed, auctionId)
     }
 
-    private fun auctionDetailsObtained(auctionDetails: AuctionDetailsResponse){
-        // Example bid call
-        // repository.bidOnAuction(::bidSuccessful, ::requestFailed, ::notEnoughValueInBid, ::auctionEnded,
-        //        auctionDetails.auction.id, (auctionDetails.auction.last_bid_value ?: auctionDetails.auction.starting_price) + 1)
-        findViewById<TextView>(R.id.auctionActivity_videoTitle).text = auctionDetails.auction.video.title
-        findViewById<TextView>(R.id.auctionActivity_bidders).text = getString(R.string.bidders_amount, auctionDetails.auctionBidders)
-        findViewById<TextView>(R.id.auctionActivity_lastBid).text = auctionDetails.auction.last_bid_value?.toString()   // fix after updating
-        findViewById<TextView>(R.id.auctionActivity_rentalDuration).text = auctionDetails.auction.rental_duration
+    private fun auctionDetailsObtained(auctionDetails: AuctionDetailsResponse) {
+        insertAuctionData(auctionDetails)
+    }
+
+    private fun bidSuccessful(auctionDetails: AuctionDetailsResponse) {
+        Toast.makeText(this, "You successfully bet", Toast.LENGTH_SHORT).show()
+        insertAuctionData(auctionDetails)
+    }
+
+    private fun notEnoughValueInBid() {
+        Toast.makeText(this, "Bid too low", Toast.LENGTH_SHORT).show()
+
+        // todo: make sure to call refresh so the current highest bid is updated
+    }
+
+    private fun auctionEnded() {
+        Toast.makeText(this, "Auction has ended", Toast.LENGTH_SHORT).show()
+        finish()
+
+        // todo: make sure to call refresh so the auction does not appear on the list
+    }
+
+    private fun requestFailed() {
+        Toast.makeText(this, "Internet connection is not stable", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun insertAuctionData(auctionDetails: AuctionDetailsResponse) {
+        findViewById<TextView>(R.id.auctionActivity_videoTitle).text =
+            auctionDetails.auction.video.title
+        findViewById<TextView>(R.id.auctionActivity_bidders).text =
+            getString(R.string.bidders_amount, auctionDetails.auctionBidders)
+        findViewById<TextView>(R.id.auctionActivity_lastBid).text =
+            auctionDetails.auction.last_bid_value?.toString()   // fix after updating
+        findViewById<TextView>(R.id.auctionActivity_rentalDuration).text =
+            auctionDetails.auction.rental_duration
 
         val expirationDateStr = auctionDetails.auction.auction_expiration_date
         val relativeDate = RelativeDate(expirationDateStr)
         findViewById<TextView>(R.id.auctionActivity_auctionExpiration).text = relativeDate.timeLeft
     }
 
-    private fun bidSuccessful(auctionDetails: AuctionDetailsResponse){
-        throw NotImplementedError()
-    }
-
-    private fun notEnoughValueInBid(){
-        throw NotImplementedError()
-    }
-
-    private fun auctionEnded(){
-        throw NotImplementedError()
-    }
-
-    private fun requestFailed(){
-        Toast.makeText(this, "Internet connection is not stable", Toast.LENGTH_SHORT).show()
+    fun onBid(view: View) {
+        val bidAmountStr = findViewById<EditText>(R.id.auctionActivity_bidAmount).text.toString()
+        if (bidAmountStr.isEmpty()) {
+            Toast.makeText(this, "Enter amount", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val bidAmount = bidAmountStr.toInt()
+        repository.bidOnAuction(
+            ::bidSuccessful, ::requestFailed, ::notEnoughValueInBid, ::auctionEnded,
+            auctionId, bidAmount
+        )
     }
 }
