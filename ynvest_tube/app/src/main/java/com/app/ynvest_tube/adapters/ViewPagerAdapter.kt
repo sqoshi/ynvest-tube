aeb4f2acdd3ed1beaa65eefbb92399924de200a2
Daniel <danielwz99@gmail.com>
2021-06-01 14:30:25 +0200

    moving fragments and fixing showing last bid value

--- ynvest_tube/app/src/main/java/com/app/ynvest_tube/adapters/ViewPagerAdapter.kt
+++ ynvest_tube/app/src/main/java/com/app/ynvest_tube/adapters/ViewPagerAdapter.kt
@@ -3,8 +3,8 @@ package com.app.ynvest_tube.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.app.ynvest_tube.fragments.auctions.AuctionListFragment
-import com.app.ynvest_tube.fragments.profile.ProfileFragment
+import com.app.ynvest_tube.fragments.AuctionListFragment
+import com.app.ynvest_tube.fragments.ProfileFragment
 import com.app.ynvest_tube.model.Auction
 
 class ViewPagerAdapter(
